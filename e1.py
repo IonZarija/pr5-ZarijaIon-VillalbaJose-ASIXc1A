c83c8d449a210ba4bfbe1fd328d44bc2275639a1
ion.zarija.7e5 <ion.zarija.7e5@itb.cat>
2024-01-22 11:47:48 +0100

    ll

---
+++ e1.py
@@ -0,0 +1,8 @@
+"""
+Ion Zarija, José Villalba
+22/01/2024
+ASIXc1A M03 UF1 A5 pr5
+"SeeTemp" Fer un programa de càlcul de temperatures del mar. Les tasques a fer són:
+    Calcular per a l’any  2022: temperatura màxima, mínima i mitjana
+    Calcular per període 2000 a 2022: temperatura màxima, mínima i mitjana
+"""
