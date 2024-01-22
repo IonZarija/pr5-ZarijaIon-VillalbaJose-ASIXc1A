e13d27c0e378f095cfeeb219a9212ff844b5bef6
ion.zarija.7e5 <ion.zarija.7e5@itb.cat>
2024-01-22 12:37:39 +0100

    ll

--- e2.py
+++ e2.py
@@ -1,3 +1,13 @@
 """
-
+Ion Zarija, José Villalba
+22/01/2024
+ASIXc1A M03 UF1 A5 pr5
+Programa que generi una llista de 100 nombres aleatoris entre 1 i 50.
+Obtenir la mitja dels nombres que es troben a les posicions parelles i la mitja del nombre de les posicions senars.
 """
+listRandom=[]
+import random
+for x in range(100):
+    print(random.randint(1,50))
+    listRandom.append()
+
